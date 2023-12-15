b7fc5b7511bc2fce7e1ccd2c48e1c7c97adf1b38
Ivan <whatyoulookingfor619@gmail.com>
2023-12-15 18:06:02 +0300

    first

---
+++ siteme/women/views.py
@@ -0,0 +1,53 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.template.loader import render_to_string
+from django.template.defaultfilters import slugify
+
+
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить статью', 'url_name': 'add_page'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'},
+]
+
+data_db = [
+    {"id": 1, 'title': 'Анджелинына Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': False},
+    {"id": 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {"id": 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
+]
+
+def index(request): # HttpRequest
+
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'posts': data_db,
+    }
+    return render(request, 'women/index.html', context=data)
+
+
+def about(request):
+    return render(request, 'women.about.html', {'title': 'О сайте', 'menu': menu})
+
+def show_post(request, post_id):
+    return  HttpResponse(f"Отображение статьи с id = {post_id}")
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+
+
+
+
+
+def add_page(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+def login(request):
+    return HttpResponse("Авторизация")
